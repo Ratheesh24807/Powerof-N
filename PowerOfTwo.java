817fdc83a30b59681d13605b5c1a1a5e197a2aba
Rateesha Attibele <Rateesha.Attibele@nielsen.com>
2022-08-26 18:27:44 +0530

    Powerof2

---
+++ PowerOfTwo.java
@@ -0,0 +1,16 @@
+package com.codebode.project;
+
+public class PowerOfTwo {
+
+	public static void main(String[] args) {
+		int num = Integer.parseInt(args[0]);
+		int result = 1;
+		
+		for(int i=1; i<=num; i++ ) {
+			result *=2;
+			System.out.println("2^" +i+  "="  +result);
+		}
+
+	}
+
+}
